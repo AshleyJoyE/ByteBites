729af479b5dcb3f1795ddc25c29dbcbe11f9f46e
Ashley Etheridge <ashleyjoyetheridge@gmail.com>
2024-02-27 19:29:35 -0500

    capable of running code!

--- byte-bites/src/Components/Home.jsx
+++ byte-bites/src/Components/Home.jsx
@@ -1,6 +1,16 @@
+import React, { useState, useEffect } from 'react';
+
 function Home () {
 
-    
+
+     // This function will called only once 
+
+    useEffect(() => {
+        // Update the document title using the browser API
+        console.log("hi")
+      });
+  
+  
     return (
         <div>
             hi

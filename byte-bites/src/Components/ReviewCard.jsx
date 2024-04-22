bf1e48565523f0b86a1a95a72cc61fa95375beeb
Ashley Etheridge <ashleyjoyetheridge@gmail.com>
2024-04-22 14:16:12 -0400

    can delete review

--- byte-bites/src/Components/ReviewCard.jsx
+++ byte-bites/src/Components/ReviewCard.jsx
@@ -1,13 +1,45 @@
 import React, { useEffect, useState } from 'react';
 import { FaStar, FaRegStar } from 'react-icons/fa';
 import styles from "./Styles/ReviewCard.module.css";
+import { useNavigate } from 'react-router-dom';
+import { AiFillDelete } from "react-icons/ai";
 
 const ReviewCard = ({ review }) => {
     const [author, setAuthor] = useState('');
+    const [isAdmin, setIsAdmin] = useState(false);
+    const [isAuthor, setIsAuthor] = useState(false);
+    const [yourId, setYourId] = useState();
+    const navigate = useNavigate();
+    const [refreshTimestamp, setRefreshTimestamp] = useState(Date.now());
+    const handleRecipeNav = () => navigate(`/Recipe/${review.recipe_id}`);
+
+    const handleDeleteReview = async () => {
+        const confirmDelete = window.confirm(`Are you sure you want to delete "${review.title}"?`);
+        if (confirmDelete) {
+            try {
+                const deleteResponse = await fetch(`http://localhost:3010/api/deleteReview/${review._id}`, {
+                    method: 'DELETE',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    }
+                });
+                if (deleteResponse.ok) {
+                    handleRecipeNav();
+                } else {
+                    console.error('Failed to delete review');
+                }
+            } catch (error) {
+                console.error('Error deleting review:', error);
+            }
+        }
+    };
+
 
     useEffect(() => {
         const fetchUsername = async () => {
             try {
+                setIsAuthor(review.reviewer_id === yourId);
+                console.log(isAuthor);
                 const userResponse = await fetch(`http://localhost:3010/api/getUserByObjectId?id=${encodeURIComponent(review.reviewer_id)}`, {
                     method: 'GET',
                     headers: {
@@ -26,9 +58,15 @@ const ReviewCard = ({ review }) => {
                 console.error('Error fetching username:', error);
             }
         };
-
+        const currentUser = localStorage.getItem("user");
+        const userId = localStorage.getItem("id");
+        const admin = localStorage.getItem("isAdmin");
+        if (currentUser && userId && admin) {
+            setYourId(userId);
+            setIsAdmin(admin === "true");
+        }
         fetchUsername();
-    }, [review.reviewer_id]);
+    }, [review.reviewer_id, refreshTimestamp]);
 
     const renderStars = (rating) => {
         const fullStars = Math.floor(rating);
@@ -48,12 +86,16 @@ const ReviewCard = ({ review }) => {
     return (
         <div className={styles.reviewCard}>
             <div className={styles.titleAndStars}>
-                <h3 className={styles.reviewTitle}>{review.title}</h3>
+                <div className={styles.div_trash_reviewTitle}>
+                    {(isAuthor || isAdmin) && <AiFillDelete className={styles.trashCan} onClick={handleDeleteReview} />}
+                    <h3 className={styles.reviewTitle}>{review.title}</h3>
+                </div>
                 <div className={styles.rating}>
                     {renderStars(review.rating)}
                 </div>
             </div>
-            <p className={styles.author}><strong>Author:</strong> {author}</p>
+            <p className={styles.author}><strong>Author:</strong> <a href={`/Profile/${review.reviewer_id}`}>@{author}</a></p>
+            {isAdmin && <label className={styles.author}>Review Object Id: {review._id}</label>}
             {
                 review.description && (
                     <div>

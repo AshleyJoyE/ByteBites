a1243bb05a43ba58afcf77d92fcc53daf6b64e66
Ashley Etheridge <ashleyjoyetheridge@gmail.com>
2024-04-23 14:57:55 -0400

    works now

--- byte-bites/src/Components/CreateRecipe.jsx
+++ byte-bites/src/Components/CreateRecipe.jsx
@@ -1,44 +1,23 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import styles from "./Styles/Home.module.css";
-import { GiSaucepan } from "react-icons/gi";
-import { FaUserCircle } from "react-icons/fa";
+import { AiFillDelete } from 'react-icons/ai'; // Import trash can icon
 import NavBar from './NavBar';
-
-import './Styles/AddRecipePage.css'; // Import CSS file for styling
+import './Styles/AddRecipePage.css';
 
 const AddRecipePage = () => {
-    
   const navigate = useNavigate();
-  const handleHomeNav = () => navigate(`/`);
   const [formData, setFormData] = useState({
     title: '',
     prepTime: '',
     cookTime: '',
-    servings: ' ',
+    serving: '',
     caloriesPerServing: '',
     description: '',
-    tags: [''],
+    tags: '',
     ingredients: [''],
-    directions:[''],
-    image: null, // Initialize image as null
+    directions: [''],
+    image: null,
   });
-
-  useEffect(() => {
-    const currentUser = localStorage.getItem("user");
-    const id = localStorage.getItem("id");
-    if (currentUser && id) {
-      setFormData(prevFormData => ({
-          ...prevFormData,
-          author_id: id // Correct syntax for setting a new property
-      }));
-    }
-    else{
-      handleHomeNav();
-    }
-  }, []);
-
-  const [recipePhoto, setRecipePhoto] = useState('');
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -54,25 +33,6 @@ const AddRecipePage = () => {
       ...formData,
       image: imageFile,
     });
-
-    // Call the image upload function when an image is selected
-    uploadRecipeImage(imageFile);
-  };
-
-  const handleTagChange = (index, value) => {
-    const updatedTags = [...formData.tags];
-    updatedTags[index] = value;
-    setFormData({
-      ...formData,
-      tags: updatedTags,
-    });
-  };
-
-  const handleAddTag = () => {
-    setFormData({
-      ...formData,
-      tags: [...formData.tags, ''],
-    });
   };
 
   const handleAddIngredient = () => {
@@ -85,6 +45,14 @@ const AddRecipePage = () => {
   const handleIngredientChange = (index, value) => {
     const updatedIngredients = [...formData.ingredients];
     updatedIngredients[index] = value;
+    setFormData({
+      ...formData,
+      ingredients: updatedIngredients,
+    });
+  };
+
+  const handleDeleteIngredient = (index) => {
+    const updatedIngredients = formData.ingredients.filter((_, i) => i !== index);
     setFormData({
       ...formData,
       ingredients: updatedIngredients,
@@ -107,109 +75,47 @@ const AddRecipePage = () => {
     });
   };
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    try {
-      // Check if the recipe photo has been uploaded
-      if (!recipePhoto) {
-        console.error('Recipe photo not uploaded');
-        return;
-      }
-
-      const response = await fetch("https://bytebites-bzpd.onrender.com/api/postRecipe", {
-          method: 'POST',
-          headers: {
-              'Content-Type': 'application/json'
-          },
-          body: JSON.stringify({
-              title: formData.title,
-              author_id: formData.author_id,
-              recipePhoto: recipePhoto,
-              description: formData.description,
-              cookTime: formData.cookTime,
-              prepTime: formData.prepTime,
-              servings: formData.servings,
-              caloriesPerServing: formData.caloriesPerServing,
-              ingredients: formData.ingredients,
-              directions: formData.directions,
-              categories: formData.categories
-          })
-      });
-
-      if (response.ok) {
-          console.log("Recipe submitted successfully!");
-          handleHomeNav();
-      } else {
-          throw new Error(`HTTP error! Status: ${response.status}`);
-      }
-    } catch (error) {
-      console.error('Error occurred during form submission:', error);
-      // Handle error here, e.g., display a message to the user
-    }
+  const handleDeleteDirection = (index) => {
+    const updatedDirections = formData.directions.filter((_, i) => i !== index);
+    setFormData({
+      ...formData,
+      directions: updatedDirections,
+    });
   };
 
-  const uploadRecipeImage = async (file) => {
-    if (!file) return;
-
-    try {
-      // Create a canvas element to resize the image
-      const canvas = document.createElement('canvas');
-      const ctx = canvas.getContext('2d');
-
-      // Load the image file
-      const img = new Image();
-      img.src = URL.createObjectURL(file);
-
-      // Wait for the image to load
-      img.onload = () => {
-        // Set canvas dimensions to 500x500 pixels
-        canvas.width = 500;
-        canvas.height = 500;
-
-        // Draw the image onto the canvas with resizing
-        ctx.drawImage(img, 0, 0, 500, 500);
-
-        // Convert canvas content to a blob
-        canvas.toBlob(async (blob) => {
-          // Create a new file from the blob
-          const resizedFile = new File([blob], file.name, { type: file.type });
-
-          // Create FormData and append the resized file
-          const formData = new FormData();
-          formData.append('fileData', resizedFile);
-          formData.append('fileName', resizedFile.name);
-
-          // Upload the resized file
-          const response = await fetch('https://bytebites-bzpd.onrender.com/api/uploadToS3', {
-            method: 'POST',
-            body: formData
-          });
-
-          if (response.ok) {
-            const data = await response.json();
-            console.log('File uploaded successfully:', data.fileUrl);
-            setRecipePhoto(data.fileUrl);
-          } else {
-            console.error('Failed to upload file to S3');
-          }
-        }, file.type);
-      };
-    } catch (error) {
-      console.error('Error uploading file:', error);
-    }
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const response = await fetch("http://localhost:3010/api/postRecipe", {
+                            method: 'POST',
+                            headers: {
+                                'Content-Type': 'application/json'
+                            },
+                            body: JSON.stringify({
+                                title: formData.title,
+                                author_id: formData.author_id,
+                                recipePhoto: formData.recipePhoto,
+                                description: formData.description,
+                                cookTime: formData.cookTime,
+                                prepTime: formData.prepTime,
+                                servings: formData.serving,
+                                caloriesPerServing: formData.caloriesPerServing,
+                                ingredients: formData.ingredients,
+                                directions: formData.directions,
+                                categories: formData.categories
+                            })
+                        });
+    // Here you can submit formData to your backend
+    console.log(formData);
   };
 
   return (
     <div className="centered-container"> 
-    <div className={styles.div_nav_bar}>
-                <NavBar>   </NavBar>
-            </div>
+      <div className="div_nav_bar">
+        <NavBar />
+      </div>
       <div className="form-container"> 
-
-
         <h2>Add Recipe</h2>
-        <form onSubmit="return false">
+        <form onSubmit={handleSubmit}>
           <div>
             <label>Title:</label>
             <input
@@ -219,7 +125,6 @@ const AddRecipePage = () => {
               onChange={handleChange}
             />
           </div>
-
           <div>
             <label>Prep Time (minutes):</label>
             <input
@@ -229,7 +134,6 @@ const AddRecipePage = () => {
               onChange={handleChange}
             />
           </div>
-
           <div>
             <label>Cook Time (minutes):</label>
             <input
@@ -239,17 +143,15 @@ const AddRecipePage = () => {
               onChange={handleChange}
             />
           </div>
-
           <div>
             <label>Servings:</label>
             <input
               type="number"
-              name="servings"
-              value={formData.servings}
+              name="serving"
+              value={formData.serving}
               onChange={handleChange}
             />
           </div>
-
           <div>
             <label>Calories per Serving:</label>
             <input
@@ -259,7 +161,6 @@ const AddRecipePage = () => {
               onChange={handleChange}
             />
           </div>
-
           <div>
             <label>Description:</label>
             <textarea
@@ -268,60 +169,55 @@ const AddRecipePage = () => {
               onChange={handleChange}
             ></textarea>
           </div>
-
           <div>
-            <label>Tags:</label>
-              {formData.tags.map((tag, index) => (
-              <div key={index}>
-              <input
-                type="text"
-                value={tag}
-                onChange={(e) => handleTagChange(index, e.target.value)}
-              />
-              </div>
-              ))}
-              <button type="button" onClick={handleAddTag}>
-                + Add Tag
-              </button>
-          </div>
-
-          <div>
-            <label>Ingredients:</label>
-              {formData.ingredients.map((ingredient, index) => (
-            <div key={index}>
+            <label>Tags (comma-separated):</label>
             <input
               type="text"
-              value={ingredient}
-              onChange={(e) => handleIngredientChange(index, e.target.value)}
+              name="tags"
+              value={formData.tags}
+              onChange={handleChange}
             />
-            </div>
+          </div>
+          <div>
+            <label>Ingredients:</label>
+            {formData.ingredients.map((ingredient, index) => (
+              <div className="ingredient-container" key={index}>
+                <input
+                  type="text"
+                  value={ingredient}
+                  onChange={(e) => handleIngredientChange(index, e.target.value)}
+                />
+                <div className="delete-button-container">
+                  <AiFillDelete onClick={() => handleDeleteIngredient(index)} />
+                </div>
+              </div>
             ))}
             <button type="button" onClick={handleAddIngredient}>
               + Add Ingredient
             </button>
           </div>
-
           <div>
             <label>Directions:</label>
-              {formData.directions.map((direction, index) => (
-              <div key={index}>
-            <textarea
-              value={direction}
-            onChange={(e) => handleDirectionChange(index, e.target.value)}
-            />
-            </div>
+            {formData.directions.map((direction, index) => (
+              <div className="direction-container" key={index}>
+                <textarea
+                  value={direction}
+                  onChange={(e) => handleDirectionChange(index, e.target.value)}
+                />
+                <div className="delete-button-container">
+                  <AiFillDelete onClick={() => handleDeleteDirection(index)} />
+                </div>
+              </div>
             ))}
             <button type="button" onClick={handleAddDirection}>
               + Add Direction
             </button>
           </div>
-
           <div>
             <label>Image:</label>
-            <input type="file" onChange={handleImageChange}/>
+            <input type="file" onChange={handleImageChange} />
           </div>
-
-          <button onClick={handleSubmit}>Submit</button>
+          <button type="submit">Submit</button>
         </form>
       </div>
     </div>
